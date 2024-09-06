d787122897b030ca0abfbcefa7dd9f015404dbfd
iliashubiitdze <shubitidzeilia7@gmail.com>
2024-09-06 16:34:13 +0400

    SeedDb

--- CR.DataAccess/Data/ApplicationDbContext.cs
+++ CR.DataAccess/Data/ApplicationDbContext.cs
@@ -12,5 +12,63 @@ namespace CR.DataAccess.Data
         }
 
         public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Car>().HasData(
+                    new Car
+                    {
+                        Id = 1,
+                        Model = "Mercedes CLS 5.5 2020",
+                        Type = "Sedan",
+                        Interior = "leathar",
+                        Fuel = "Gasoline",
+                        Transmission = "Authomatic",
+                        Seats = "4",
+                        Price = 100,
+                        PriceFor8 = 80
+                    },
+
+                    new Car
+                    {
+                        Id = 2,
+                        Model = "Mazda 6 2.0 2016",
+                        Type = "Sedan",
+                        Interior = "Fabric",
+                        Fuel = "Gasoline",
+                        Transmission = "Authomatic",
+                        Seats = "5",
+                        Price = 70,
+                        PriceFor8 = 50
+                    },
+                    new Car
+                    {
+                        Id = 3,
+                        Model = "BMW X5 4.0 2018",
+                        Type = "Sedan",
+                        Interior = "leathar",
+                        Fuel = "Gasoline",
+                        Transmission = "Mechanical",
+                        Seats = "4",
+                        Price = 90,
+                        PriceFor8 = 70
+                    },
+                    new Car
+                    {
+                        Id = 4,
+                        Model = "Mecedes Viano 2.5 2021",
+                        Type = "Van",
+                        Interior = "Fabric",
+                        Fuel = "Diesel",
+                        Transmission = "Authomatic",
+                        Seats = "7",
+                        Price = 120,
+                        PriceFor8 = 100
+                    }
+
+                );
+        }
     }
 }
---
+++ CR.DataAccess/Migrations/20240906123319_SeedDb.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CR.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "EndDate", "Fuel", "Interior", "Model", "Price", "PriceFor8", "Seats", "StartingDate", "Transmission", "Type" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(1, 1, 1), "Gasoline", "leathar", "Mercedes CLS 5.5 2020", 100.0, 80.0, "4", new DateOnly(1, 1, 1), "Authomatic", "Sedan" },
+                    { 2, new DateOnly(1, 1, 1), "Gasoline", "Fabric", "Mazda 6 2.0 2016", 70.0, 50.0, "5", new DateOnly(1, 1, 1), "Authomatic", "Sedan" },
+                    { 3, new DateOnly(1, 1, 1), "Gasoline", "leathar", "BMW X5 4.0 2018", 90.0, 70.0, "4", new DateOnly(1, 1, 1), "Mechanical", "Sedan" },
+                    { 4, new DateOnly(1, 1, 1), "Diesel", "Fabric", "Mecedes Viano 2.5 2021", 120.0, 100.0, "7", new DateOnly(1, 1, 1), "Authomatic", "Van" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
